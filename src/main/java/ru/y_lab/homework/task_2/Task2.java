2ba7b375387e4779e4ad706eac78ac5732f9f270
Vlad <VladBashlakov@gmail.com>
2022-08-28 02:21:29 +0300

    task_2

---
+++ src/main/java/ru/y_lab/homework/task_2/Task2.java
@@ -0,0 +1,57 @@
+package ru.y_lab.homework.task_2;
+
+import java.util.Arrays;
+
+public class Task2 {
+
+
+    public static void main(String[] args) {
+        int[] arr = {5, 6, 3, 2, 5, 1, 4, 9};
+        mergeSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+
+
+    public static void merge(int[] arr, int[] left, int[] right) {
+
+        int mainP = 0;
+        int leftP = 0;
+        int rightP = 0;
+        while (leftP < left.length && rightP < right.length) {
+            if (left[leftP] <= right[rightP]) {
+                arr[mainP++] = left[leftP++];
+            } else {
+                arr[mainP++] = right[rightP++];
+            }
+        }
+        while (leftP < left.length) {
+            arr[mainP++] = left[leftP++];
+        }
+        while (rightP < right.length) {
+            arr[mainP++] = right[rightP++];
+        }
+    }
+
+    public static void mergeSort(int[] arr) {
+        if (arr.length < 2) {
+            return;
+        }
+        int mid = arr.length / 2;
+        int[] left = new int[mid];
+        int[] right = new int[arr.length - mid];
+
+        for (int i = 0; i < left.length; i++) {
+            left[i] = arr[i];
+        }
+
+        for (int i = 0; i < right.length; i++) {
+            right[i] = arr[mid + i];
+        }
+
+        mergeSort(left);
+        mergeSort(right);
+
+        merge(arr, left, right);
+    }
+
+}
