f0439314a039f73605e4506a01c30de9e50d7e0c
Vlad <VladBashlakov@gmail.com>
2022-09-08 23:07:52 +0300

    Сделан 2 таск

---
+++ src/main/java/ru/y_lab/homework_2/Task_2.java
@@ -0,0 +1,26 @@
+package ru.y_lab.homework_2;
+
+public class Task_2 {
+       /*
+        Task2
+
+            [3, 4, 2, 7], 10 -> [3, 7] - вывести пару именно в скобках, которые дают сумму - 10
+         */
+
+
+    public static void main(String[] args) {
+
+        int[] arr = {3, 4, 2, 7};
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int i1 = i + 1; i1 < arr.length; i1++) {
+                if (arr[i1] + arr[i] == 10) {
+                    System.out.println("[" + arr[i] + ", " + arr[i1] + "]");
+                    i = arr.length - 1;
+                }
+            }
+        }
+    }
+
+
+}
